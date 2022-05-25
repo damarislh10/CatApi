9f166d64c4d4cde03747fee5c1181d5604f2b5a4
Jose David <josedbolivarmafrontend@gmail.com>
2022-05-25 10:42:23 -0500

    Dinamic Routes

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import AppRouter from './routes/AppRouter';
 
 ReactDOM.render(
-    <App />,
+    <AppRouter />,
     document.getElementById('root')
 );
